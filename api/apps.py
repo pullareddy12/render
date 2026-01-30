64a5d2d5c43534237b922d2d1f8393726c4c44db
Jaswanth Simha <jaswanthsimha2004@gmail.com>
2026-01-30 16:06:58 +0530

    temporary superuser creation for render free plan

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+import os
 
 
 class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        if os.environ.get("RENDER") == "true":
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="jashu",
+                    email="jaswanthsimha@gmail.com",
+                    password="Jashu@2004"
+                )
--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    "api",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
