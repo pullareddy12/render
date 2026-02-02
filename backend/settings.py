20871848f3d83f2670b3281b1e6a82e266b27c3a
Pullareddy <pullareddyanakala7@gmail.com>
2026-02-02 11:21:00 +0530

    Initial Django backend setup for Render

--- backend/settings.py
+++ backend/settings.py
@@ -4,7 +4,8 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
+
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
@@ -51,7 +52,7 @@ TEMPLATES = [
         },
     },
 ]
-
+'''
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
@@ -68,7 +69,29 @@ else:
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
+    }'''
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "").strip()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
+if DATABASE_URL:
+    try:
+        DATABASES["default"] = dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=DATABASE_URL.startswith("postgres"),
+        )
+    except Exception as e:
+        print("⚠️ Invalid DATABASE_URL:", DATABASE_URL)
+        print("⚠️ Reason:", e)
+
+
 
 
 REST_FRAMEWORK = {
@@ -107,3 +130,4 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
